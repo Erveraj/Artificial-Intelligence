6a4e3340f95669b1ced9a73a0a8b9f031b15a543
Er. Varinder Singh <varinder.sgc.er@gmail.com>
2026-08-10 15:51:44 +0530

    Implement Naïve Bayes Classifier, Compute Accuracy and Visualize Performance

--- PRO6.py
+++ PRO6.py
@@ -1,4 +1,4 @@
-# Program no 7: Implement Naïve Bayes Classifier, Compute Accuracy and Visualize Performance
+# Program no 6: Implement Naïve Bayes Classifier, Compute Accuracy and Visualize Performance
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
