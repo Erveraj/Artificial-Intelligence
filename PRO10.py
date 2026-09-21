38094d378f70f0ae18a87c025d2c8525f1149951
Everaj <varinder.sgc.er@gmail.com>
2026-09-21 16:08:58 +0530

    Fix Error

--- PRO10.py
+++ PRO10.py
@@ -1,5 +1,7 @@
-#Program No 10: Tokenize Text Data Using Python
+# Program No 10: Tokenize Text Data Using Python
+
 import nltk
+
 from nltk.tokenize import word_tokenize, sent_tokenize
 
 nltk.download('punkt')
@@ -8,6 +10,7 @@ nltk.download('punkt_tab')
 text = "Artificial Intelligence is powerful. NLP processes human language."
 
 sentences = sent_tokenize(text)
+
 words = word_tokenize(text)
 
 print("Sentence Tokens:")
